2c17305e7d8bf269fb20f16467f6fe88960b160a
Siddhi <siddhipatel2610@gmail.com>
2020-02-05 15:13:11 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RequirementDetailsComponent } from './requirement-details/requirement-details.component';
+import { DemoMaterialModule } from '../material-modules';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequirementDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/requirement-details/requirement-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-requirement-details',
+  templateUrl: './requirement-details.component.html',
+  styleUrls: ['./requirement-details.component.scss']
+})
+export class RequirementDetailsComponent implements OnInit {
+
+  requirementsData : FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.requirementsData = this.formBuilder.group({
+      jobTitle:['', Validators.required],
+      jobDiscription: ['', Validators.required],
+      experienceRequired: ['', Validators.required],
+      salary: ['',Validators.required]
+    })
+   }
+
+   onSave(){
+     //whatever you want to do with the data
+   }
+
+  ngOnInit() {
+  }
+
+}
